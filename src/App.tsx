95db1359b28d380064bd53dcf92c118b61fd9ef8
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-16 10:01:05 +0530

    Initialize the React project and build the login and authentication UI with validations

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Toaster } from "react-hot-toast";
+import AppRouter from "@/routes/AppRouter";
+
+function App() {
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
