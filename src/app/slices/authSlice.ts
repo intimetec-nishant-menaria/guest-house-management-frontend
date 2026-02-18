f80a32611ab0eb10ebec1eb64b3155f2edfd7648
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-18 13:44:37 +0530

    Remove all the unwanted commented code

--- src/app/slices/authSlice.ts
+++ src/app/slices/authSlice.ts
@@ -1,7 +1,5 @@
-// import { loginUser } from "@/app/asyncThunk/authThunk";
 import type { AuthState } from "@/utils/interfaces/authState";
 import { createSlice } from "@reduxjs/toolkit";
-// import { resetPassword } from "@/app/asyncThunk/authThunk";
 
 const initialState: AuthState = {
   user: null,
@@ -14,32 +12,6 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {}
-  // extraReducers: (builder) => {
-  //   builder
-  //     .addCase(loginUser.pending, (state) => {
-  //       state.loading = true;
-  //       state.error = null;
-  //     })
-  //     .addCase(loginUser.fulfilled, (state, action) => {
-  //       state.loading = false;
-  //       state.user = action.payload;
-  //     })
-  //     .addCase(loginUser.rejected, (state, action) => {
-  //       state.loading = false;
-  //       state.error = action.payload as string;
-  //     })
-  //     .addCase(resetPassword.pending, (state) => {
-  //       state.loading = true;
-  //       state.error = null;
-  //     })
-  //     .addCase(resetPassword.fulfilled, (state) => {
-  //       state.loading = false;
-  //     })
-  //     .addCase(resetPassword.rejected, (state, action) => {
-  //       state.loading = false;
-  //       state.error = action.payload as string;
-  //     });
-  // },
 });
 
 export default authSlice.reducer;
